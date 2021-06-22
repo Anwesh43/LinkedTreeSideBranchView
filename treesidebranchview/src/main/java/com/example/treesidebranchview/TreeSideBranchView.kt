42d4397ad6bb2d49afbeee2f75f5f2a4b15ed6c2
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 02:29:02 +0530

    adding TreeeSideBranchView class

--- treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
+++ treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawTSBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawTreeSideBranch(scale, w, h, paint)
 }
+
+class TreeSideBranchView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
