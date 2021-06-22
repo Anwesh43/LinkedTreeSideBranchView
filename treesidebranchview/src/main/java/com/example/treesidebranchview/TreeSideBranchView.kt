698653268980b955bc5556410f5f9773d4c225e7
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 01:49:01 +0530

    adding constants and libraries

--- treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
+++ treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
@@ -1,2 +1,26 @@
 package com.example.treesidebranchview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val rFactor : Float = 29.3f
+val backColor : Int = Color.parseColor("#BDBDBD")
