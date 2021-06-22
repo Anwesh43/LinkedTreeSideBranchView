c9d012246ae7ac48f24acb6d7a3be28a46097b1a
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 02:37:50 +0530

    adding TSBNode class

--- treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
+++ treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
@@ -128,4 +128,45 @@ class TreeSideBranchView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TSBNode(var i : Int, val state : State = State()) {
+
+        private var next : TSBNode? = null
+        private var prev : TSBNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TSBNode {
+            var curr : TSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
