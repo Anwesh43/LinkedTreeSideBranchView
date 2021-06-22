987e34b0ad34495dad07dd2e55a929877ac42eaf
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 02:27:48 +0530

    adding Canvas extension methods

--- treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
+++ treesidebranchview/src/main/java/com/example/treesidebranchview/TreeSideBranchView.kt
@@ -24,7 +24,44 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val rFactor : Float = 29.3f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val branches : Int = 12
+
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this -  i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTreeSideBranch(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val gap : Float = (h * 0.8f)  / branches
+    save()
+    drawLine(0f, h - h * sc5, 0f, h - h * sc1, paint)
+    for (j in 0..(10)) {
+        val sk : Float = 1f - 2 * (j % 2)
+        save()
+        translate(w / 2 + (w / 2 + size) * sc4 * sk, h * 0.1f + gap * j)
+        drawLine(0f, 0f, size * sk * sc2, 0f, paint)
+        drawCircle(
+            size * sk * Math.floor(sc2.toDouble()).toFloat(),
+            0f,
+            r * sc3,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    drawTreeSideBranch(scale, w, h, paint)
+}
